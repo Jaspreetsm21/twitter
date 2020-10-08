536221bcb59de80a47b8f2693f68cc4ff4c83f8f
Jaspreet singh <60583082+Jaspreetsm21@users.noreply.github.com>
2020-10-08 10:33:38 +0100

    Update views.py

--- base/views.py
+++ base/views.py
@@ -29,7 +29,7 @@ def home(request):
         t1 = tk
     
         twttt_created.append(tk)
-    kk =(twttt_created)
+    kk =(twttt_created[-1])
     #print(kk)
 
     consumer_key = 'WURYy5DNwdoKNuq36C2ME9MG5'
@@ -165,7 +165,7 @@ def get_data(request):
         t1 = tk
     
         twttt_created.append(tk)
-    kk =(twttt_created)
+    kk =(twttt_created[-1])
     #print(kk)
 
     consumer_key = 'WURYy5DNwdoKNuq36C2ME9MG5'
