1bd11f7c73743394ce88898d91e04e8eee1fd8eb
Jaspreet singh <60583082+Jaspreetsm21@users.noreply.github.com>
2020-10-09 11:46:45 +0100

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('dates=30d', views.get_data30, name='dates=30d'),
+    path('date30d', views.date30d, name='date30d'),
+    path('date14d', views.date14d, name='date14d'),
+    path('date7d', views.date7d, name='date7d'),
     path('chart', views.get_data, name='chart'),
+    path('chart2', views.get_data30, name='chart2'),
+    path('chart3', views.get_data14, name='chart3'),
+    path('chart4', views.get_data7, name='chart4'),
 ]
